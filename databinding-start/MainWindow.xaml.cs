cc8be9bb69a17b8f715143b294c9bab491912845
mhuseynn <hmehdiyev2005@gmail.com>
2023-10-30 19:56:19 +0400

    final

---
+++ databinding-start/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace databinding_start
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Message> messages { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            messages = new()
+            {
+                new("Salam",DateTime.Now),
+                new("Necesiz",DateTime.Now),
+                 new("How old are you",DateTime.Now),
+                new("How are you",DateTime.Now),
+                 new("Whta are you doing",DateTime.Now),
+                new("i am fine",DateTime.Now),
+                 new("Thanks",DateTime.Now),
+                new("you?",DateTime.Now),
+                 new("Salam",DateTime.Now),
+                new("Salam",DateTime.Now)
+            };
+            DataContext = this;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ databinding-start/Message.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace databinding_start
+{
+    public class Message
+    {
+        
+        public Message(string text, DateTime date)
+        {
+            this.text = text;
+            this.date = date;
+        
+        }
+
+        public string text { get; set; }
+
+        public DateTime date { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{text}\t\t{date.TimeOfDay.Hours.ToString()}:{date.TimeOfDay.Minutes.ToString()}";
+              
+        }
+
+
+    }
+}
